deef3f699644b3f73bdaa708faef87fd2c57b0a2
Dave Tarrant <davetaz@theodi.org>
2018-01-26 16:01:01 +0000

    Fixing??

--- demo8/templates.js
+++ demo8/templates.js
@@ -370,6 +370,22 @@ this["Handlebars"]["templates"]["shadow"] = Handlebars.template({"compiler":[7,"
     return "<div id=\"shadow\" class=\"shadow display-none\"></div>";
 },"useData":true});
 
+this["Handlebars"]["templates"]["dataFetch"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
+    return "tabindex=\"0\"";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=container.escapeExpression, alias2=depth0 != null ? depth0 : (container.nullContext || {});
+
+  return "<div class=\"dataFetch-inner component-inner\" role=\"region\" aria-label=\""
+    + alias1(container.lambda(((stack1 = ((stack1 = ((stack1 = (depth0 != null ? depth0._globals : depth0)) != null ? stack1._components : stack1)) != null ? stack1._dataFetch : stack1)) != null ? stack1.ariaRegion : stack1), depth0))
+    + "\" "
+    + ((stack1 = helpers["if"].call(alias2,((stack1 = ((stack1 = ((stack1 = (depth0 != null ? depth0._globals : depth0)) != null ? stack1._components : stack1)) != null ? stack1._dataFetch : stack1)) != null ? stack1.ariaRegion : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ">\n"
+    + ((stack1 = container.invokePartial(partials.component,depth0,{"name":"component","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + "    <form id=\"dataSources\">\n    	"
+    + alias1(((helper = (helper = helpers.dataSource || (depth0 != null ? depth0.dataSource : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias2,{"name":"dataSource","hash":{},"data":data}) : helper)))
+    + "\n    </div>\n</div>";
+},"usePartial":true,"useData":true});
+
 this["Handlebars"]["templates"]["graphic"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     return " graphic-widget-attribution";
 },"3":function(container,depth0,helpers,partials,data) {
